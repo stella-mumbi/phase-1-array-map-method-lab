bf7dba9c5947a892c40417bd9b4380a66644ad31
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-24 23:57:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  // ... rest of the tutorial titles
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(' '); // Split the title into individual words
+    const capitalizedWords = words.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    ); // Capitalize the first letter of each word
+    return capitalizedWords.join(' '); // Join the words back into a title
+  });
 }
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
